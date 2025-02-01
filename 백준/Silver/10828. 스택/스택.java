8035d7beead163aa06d809dd40933543d42c8e9b
Hyochang098 <wlsgyckd7@naver.com>
2025-02-01 11:24:23 +0900

    [Silver IV] Title: 스택, Time: 492 ms, Memory: 39616 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -13,22 +13,22 @@ public class Main {
     	
     	for(int i =0;i<n; i++) {
     		String str = sc.nextLine();
-    		if(str.startsWith("push")) {
-    			int a = Integer.parseInt(str.split(" ")[1]);
-    			stack.push(a);
+    		if(str.startsWith("push")) {//push로 시작하면
+    			int a = Integer.parseInt(str.split(" ")[1]);//제일 뒤의 숫자를 정수로 변환
+    			stack.push(a);//stack a넣기
     		}else if(str.equals("top")) {
-    			if (stack.empty())
+    			if (stack.empty())//스택이 비어있는지 확인
     				System.out.println(-1);
     			else
-    			System.out.println(stack.peek());
+    			System.out.println(stack.peek());//스택의 제일 위의 값을 출력만 제거 x
     		}else if(str.equals("size")) {
-    			System.out.println(stack.size());
+    			System.out.println(stack.size());//스택의 크기
     		}else if(str.equals("empty")) {
-    			if (stack.empty())
+    			if (stack.empty())//스택이 비어있는지
     				System.out.println(1);
     			else
     				System.out.println(0);
-    		}else if(str.equals("pop")) {
+    		}else if(str.equals("pop")) {//스택의 제일 위의 값을 출력 후 제거
     			if (stack.empty())
     				System.out.println(-1);
     			else
