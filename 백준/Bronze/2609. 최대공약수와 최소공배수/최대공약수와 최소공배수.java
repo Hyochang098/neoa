b9700152553bd2d127f93732a2a949f7b386f71a
Hyochang098 <wlsgyckd7@naver.com>
2025-02-06 01:13:51 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int a = sc.nextInt();
 		int k = sc.nextInt();
+		int n = Math.max(a,k);
+		k = Math.min(a,k);
 		int large = Ucl(n,k);//최대공약수
 		int Small = n*k/large;
 		System.out.println(large);
