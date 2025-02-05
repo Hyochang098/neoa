9b6e0e13da027840ff020e893d4e2400f14d7af9
Hyochang098 <wlsgyckd7@naver.com>
2025-02-06 00:55:12 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 18272 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -6,20 +6,15 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		double[] arr = new double[a];
 		double max = 0;
+		int sum = 0;
 		for (int i = 0; i < a; i++) {
-			arr[i] = sc.nextInt();
-			if (arr[i] > max)
-				max = arr[i];
+			int score= sc.nextInt();
+			sum+=score;
+			if (score> max)
+				max = score;
 		}
-		double sum = 0;
-		for (int i = 0; i < a; i++) {
-			arr[i] = (arr[i] / max) * 100;
-			sum +=arr[i];
-		}
-
-		double ans =  (sum/a);
+		double ans =  (sum/(a*max)*100);
 		System.out.print(ans);
 	}
 
