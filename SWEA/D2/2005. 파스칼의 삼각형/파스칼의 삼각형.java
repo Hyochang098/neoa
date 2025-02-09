4da239f8db7620d0ac28e0bc67d0f01b8c482cf4
Hyochang098 <wlsgyckd7@naver.com>
2025-02-09 15:47:01 +0900

    [D2] Title: 파스칼의 삼각형, Time: 99 ms, Memory: 27,264 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,31 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= t; tc++) {
-			int n = sc.nextInt()-1;
-			ArrayList<Integer> pre = new ArrayList<Integer>();
+			List<Integer> pre = new ArrayList<>();
+			int n = Integer.parseInt(br.readLine())-1;
+			pre.add(0);
 			pre.add(1);
-			System.out.println("#" + tc);
-			System.out.println(1);
+			pre.add(0);
+			sb.append("#").append(tc+"\n");
+			sb.append(1).append("\n");
 			for (int i = 0; i < n; i++) {
-				ArrayList<Integer> cur = new ArrayList<>();
-				cur.add(1);
-				for (int j = 1; j < pre.size(); j++) {
-					cur.add(pre.get(j - 1) + pre.get(j));
+				List<Integer> cur = new ArrayList<>();
+				cur.add(0);
+				for(int j=0;j<pre.size()-1;j++) {
+					cur.add(pre.get(j)+pre.get(j+1));
+					sb.append(pre.get(j)+pre.get(j+1)+" ");
 				}
-				cur.add(1);
-
+				cur.add(0);
 				pre = cur;
-
-				for (int j = 0; j < pre.size(); j++) {
-					System.out.print(pre.get(j) + " ");
-				}
-				System.out.println();
+				sb.append("\n");
 			}
+			
 		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
