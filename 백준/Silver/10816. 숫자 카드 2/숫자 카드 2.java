d0ab8ceab4cba6c3cc7e8f8b4a9fa51a3caf10cb
Hyochang098 <wlsgyckd7@naver.com>
2025-02-11 10:53:48 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 816 ms, Memory: 196752 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,24 +1,36 @@
-import java.io.*;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int m = Integer.parseInt(br.readLine());
-        HashMap<Integer, Integer> checklist = new HashMap<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            checklist.put(num, checklist.getOrDefault(num, 0) + 1);
-        }
-        int n = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int checking = Integer.parseInt(st.nextToken());
-            sb.append(checklist.getOrDefault(checking, 0)).append(" ");
-        }
-        System.out.print(sb.toString().trim());
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] checking = new int[20000001];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < n; i++) {
+			int temp = Integer.parseInt(st.nextToken())+10000000;
+			checking[temp]++;
+		}
+
+		int m = Integer.parseInt(br.readLine());
+		StringTokenizer st1 = new StringTokenizer(br.readLine()); 
+		for (int i = 0; i < m; i++) {
+			int temp = Integer.parseInt(st1.nextToken())+10000000;
+			sb.append(checking[temp]).append(" ");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
