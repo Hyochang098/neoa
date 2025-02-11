708f103538e3b3da46e534e96e7e61b8349d8c50
Hyochang098 <wlsgyckd7@naver.com>
2025-02-11 22:51:20 +0900

    [Bronze I] Title: 팰린드롬수, Time: 168 ms, Memory: 17572 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+
         
-        while (!str.equals("0")) { 
+        while (true) {//종료 조건
+            String str=sc.nextLine();
+            if(str.equals("0"))
+            break;
+            
             boolean felindron = true;
             
-            for (int i = 0; i < (str.length() / 2); i++) {
-                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+            for (int i = 0; i < (str.length() / 2); i++) {//문자열의 시작과 끝에서 올 거니까 반으로만
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {//서로 다르면 false로
                     felindron = false;
-                    break;
+                    break;//다르면 탐색 종료
                 }
             }
             
@@ -20,7 +24,6 @@ public class Main {
             else
                 System.out.println("no");
             
-            str = sc.nextLine(); 
         }
 
     }
