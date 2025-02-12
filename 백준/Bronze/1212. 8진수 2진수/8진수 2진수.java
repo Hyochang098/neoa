ddfdbdf57f37c611cddc4da434a1f87eed837f3c
Hyochang098 <wlsgyckd7@naver.com>
2025-02-13 01:40:05 +0900

    [Bronze II] Title: 8진수 2진수, Time: 500 ms, Memory: 31012 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -3,84 +3,70 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		String str = sc.nextLine();
 
 		if (str.equals("0")) {
 			System.out.println(0);
 			return;
 		}
-		int[] arr = new int[str.length()];
 
+		int[] arr = new int[str.length()];
 		for (int i = 0; i < str.length(); i++) {
 			arr[i] = str.charAt(i) - '0';
 		}
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(exchange1(arr[0]));
 		for (int i = 1; i < str.length(); i++) {
 			sb.append(exchange(arr[i]));
 		}
-		System.out.println(sb.toString());
+
+		System.out.println(sb);
 	}
 
 	static String exchange(int a) {
-		String result = "";
 		switch (a) {
 		case 0:
-			result = "000";
-			break;
+			return "000";
 		case 1:
-			result = "001";
-			break;
+			return "001";
 		case 2:
-			result = "010";
-			break;
+			return "010";
 		case 3:
-			result = "011";
-			break;
+			return "011";
 		case 4:
-			result = "100";
-			break;
+			return "100";
 		case 5:
-			result = "101";
-			break;
+			return "101";
 		case 6:
-			result = "110";
-			break;
+			return "110";
 		case 7:
-			result = "111";
-			break;
+			return "111";
+		default:
+			return "error";
 		}
-		return result;
-
 	}
 
 	static String exchange1(int a) {
-		String result = "";
 		switch (a) {
+		case 0:
+			return "0";
 		case 1:
-			result = "1";
-			break;
+			return "1";
 		case 2:
-			result = "10";
-			break;
+			return "10";
 		case 3:
-			result = "11";
-			break;
+			return "11";
 		case 4:
-			result = "100";
-			break;
+			return "100";
 		case 5:
-			result = "101";
-			break;
+			return "101";
 		case 6:
-			result = "110";
-			break;
+			return "110";
 		case 7:
-			result = "111";
-			break;
+			return "111";
+		default:
+			return "error";
 		}
-		return result;
-
 	}
 }
