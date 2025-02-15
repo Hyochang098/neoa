4dff7d9329e14d89e275c614a91e1be0ff792d42
Hyochang098 <wlsgyckd7@naver.com>
2025-02-15 12:18:10 +0900

    [D2] Title: 풍선팡, Time: 332 ms, Memory: 62,300 KB -BaekjoonHub

---
+++ SWEA/D2/9490. 풍선팡/README.md
@@ -0,0 +1,15 @@
+# [D2] 풍선팡 - 9490 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXAerAPaVXMDFARP) 
+
+### 성능 요약
+
+메모리: 62,300 KB, 시간: 332 ms, 코드길이: 929 Bytes
+
+### 제출 일자
+
+2025-02-15 12:18
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/9490. 풍선팡/풍선팡.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+		for (int tc = 1; tc <= t; tc++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			int[][] poong = new int[n][m];
+
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					poong[i][j] = sc.nextInt();
+				}
+			}
+
+			int[] dy = { 1, -1, 0, 0 };
+			int[] dx = { 0, 0, 1, -1 };
+
+			int ans = Integer.MIN_VALUE;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < m; j++) {
+					int paper = poong[i][j];
+					for (int k = 0; k < 4; k++) {
+						for (int o = 1; o <= poong[i][j]; o++) {
+							int ny = i + dy[k]*o;
+							int nx = j + dx[k]*o;
+							if (0 <= ny && ny < n && 0 <= nx && nx < m) {
+								paper += poong[ny][nx];
+							}
+						}
+					}
+					ans = Math.max(ans, paper);
+				}
+			}
+			System.out.println("#" + tc + " " + ans);
+		}
+	}
+
+}
