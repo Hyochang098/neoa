9b7e8cd2404d2d756076608b4f99f51a96a6ea57
Hyochang098 <wlsgyckd7@naver.com>
2025-02-18 23:26:53 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 268 ms, Memory: 25064 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -6,37 +6,40 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        int[] salary = new int[n];
+		st = new StringTokenizer(br.readLine());
+		int[] salary = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            salary[i] = Integer.parseInt(st.nextToken());
-        }
+		for (int i = 0; i < n; i++) {
+			salary[i] = Integer.parseInt(st.nextToken());
+		}
 
-        if (m == 0) {
-            System.out.println(0);
-            return;
-        }
+		if (m == 0) {
+			System.out.println(0);
+			return;
+		}
 
-        long sum = 0;
-        for (int i = 0; i < m; i++) {
-            sum += salary[i];
-        }
-        long junsu = sum;
+		long sum = 0;
+		for (int i = 0; i < m; i++) {
+			sum += salary[i];
+		}
 
-        for (int i = m; i < n; i++) {
-            sum += salary[i];
-            sum -= salary[i - m];
-            junsu = Math.max(junsu, sum);
-        }
-        System.out.println(junsu);
-    }
+		long junsu = sum;
+
+		for (int i = m; i < n; i++) {
+			sum += salary[i];
+			sum -= salary[i - m];
+			junsu = Math.max(junsu, sum);
+		}
+		bw.write(String.valueOf(junsu));
+		bw.flush();
+		bw.close();
+	}
 }
