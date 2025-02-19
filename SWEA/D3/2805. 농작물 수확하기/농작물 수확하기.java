0f14d98535d1eeb40ceef192d413516f264e5a74
Hyochang098 <wlsgyckd7@naver.com>
2025-02-19 14:43:51 +0900

    [D3] Title: 농작물 수확하기, Time: 121 ms, Memory: 33,728 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -3,30 +3,40 @@ import java.util.Scanner;
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int t = sc.nextInt();
 		for (int tc = 1; tc <= t; tc++) {
 			int n = sc.nextInt();
 			sc.nextLine();
 
-			int k = n / 2;
 			int[][] farm = new int[n][n];
-
-			int ans = 0;
 			for (int i = 0; i < n; i++) {
-				String temp = sc.nextLine();
+				String str = sc.nextLine();
 				for (int j = 0; j < n; j++) {
-					farm[i][j] = temp.charAt(j)-'0';
+					farm[i][j] = str.charAt(j) - '0';
 				}
 			}
-			for (int i = 0; i < n; i++) {
-                int start = Math.abs(k - i);
-                int end = n - start;
 
-                for (int j = start; j < end; j++) {
-                    ans += farm[i][j];
-                }
-            }
-			System.out.println("#"+tc+" "+ans);
+			int center = n / 2;
+
+			int step = 0;
+			int ans = 0;
+			while (step <= center) {
+			    int left = center - step;
+			    int right = center + step;
+
+			    
+			    for (int i = left; i <= right; i++) {
+			        ans += farm[step][i];
+			        if (step != center) {
+			            ans += farm[n - 1 - step][i];
+			        }
+			    }
+
+			    step++; 
+			}
+
+			System.out.println("#" + tc + " " + ans);
 		}
 	}
-}
+} 
