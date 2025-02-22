ff42c1585063055c80c2a050ce49eac8fd0d5482
Hyochang098 <wlsgyckd7@naver.com>
2025-02-22 14:55:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1724 ms, Memory: 208220 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -14,7 +16,7 @@ public class Main {
 		List<List<Integer>> tree = new ArrayList<>();
 
 		for (int i = 0; i < n + 1; i++) {
-			tree.add(new ArrayList<>());//각 줄에 n만큼의 리스트를 생성
+			tree.add(new ArrayList<>());// 각 줄에 n만큼의 리스트를 생성
 		}
 
 		for (int i = 0; i < n - 1; i++) {
@@ -23,25 +25,42 @@ public class Main {
 			tree.get(node1).add(node2);
 			tree.get(node2).add(node1);
 		}
-		
-		
 
 		int[] ans = new int[n + 1];
-		findup(1, tree, ans);
-
+//		findup(1, tree, ans);
+		findup(tree, ans);
 		for (int i = 2; i < ans.length; i++) {
 			System.out.println(ans[i]);
 		}
 
 	}
 
-	//부모찾기(해당 노드, 트리, 부모 체크)
-	static void findup(int cur, List<List<Integer>> tree, int[] check) {
-		for (int a : tree.get(cur)) {// 해당 노드에 연결된 모든 노드 호출
-			if (check[a] == 0) {// 체크 배열 값이 0=>방문하지 않음+부모를 못찾음
-				check[a] = cur;// 해당하는 부모를 넣음
-				findup(a, tree, check);// 부모 노드도 다시 재귀로
+	//bfs로 탐색
+	//1이 루트이므로 1을 넣고 시작
+	static void findup(List<List<Integer>> tree, int[] check) {
+		Queue<Integer> que = new LinkedList<>();
+		que.add(1);
+		check[1] = 1;
+		while (!que.isEmpty()) {
+			int cur = que.poll();
+
+			for (int pre : tree.get(cur)) {
+				if (check[pre] == 0) {
+					check[pre] = cur;
+					que.add(pre);
+				}
 			}
 		}
+
 	}
+
+	// 부모찾기(해당 노드, 트리, 부모 체크)
+//	static void findup(int cur, List<List<Integer>> tree, int[] check) {
+//		for (int a : tree.get(cur)) {// 해당 노드에 연결된 모든 노드 호출
+//			if (check[a] == 0) {// 체크 배열 값이 0=>방문하지 않음+부모를 못찾음
+//				check[a] = cur;// 해당하는 부모를 넣음
+//				findup(a, tree, check);// 부모 노드도 다시 재귀로
+//			}
+//		}
+//	}
 }
