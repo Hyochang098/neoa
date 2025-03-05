1abe4d6ddcf40b28b0202d6bd29b30925dc78ef8
Hyochang098 <wlsgyckd7@naver.com>
2025-03-05 21:33:30 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 252 ms, Memory: 21068 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		// dp 배열에는 arr[i]가 가질 수 있는 가질 수 있는 배열의 길이
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+			dp[i] = 1;
+		}
+
+		// 부분 수열 뽑음
+		// i는1부터 시작해서
+		// j는 0부터 i까지
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i]) {//현재 고른 수보다 작으면 +1을 함
+					dp[i] = Math.max(dp[i], dp[j] + 1);//dp[j]는 arr[j]보다 작은 애들의 값임
+				}
+			}
+
+		}
+		int max = 0;
+		for (int i = 0; i < n; i++) {//배열에서 최댓값이 최고 길이
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}
+}
