c7fd17f72109ea2649ac753a32ecede2856f4103
Hyochang098 <wlsgyckd7@naver.com>
2025-03-06 01:45:52 +0900

    [Silver III] Title: 계단 오르기, Time: 188 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,35 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	// 2579 - 계단 오르기
+	// 시작점에서부터 규칙에 따라 계단을 올라갓을 때의 최댓값을 구하기
 
-        int n = sc.nextInt();
-        int[] steps = new int[n + 1];
-        int[] dp = new int[n + 1];
+	// 그냥 갈때마다 최댓값이 있는 쪽으로 가야하나?
+	// 연속 세번인지 확인하는 변수가 있어야하나?
+	// 그냥 값 입력을 받고 이전 분기에 값 받은애 안받은애 하고
+	// 받은애는 3번 연속인지 확인해야할듯?
+	// 걍 화분처럼 두줄로 내려가는 배열이면될거같은데->실패...
+	// DP배열을 만듬 배열에 들어갈 값은 밟았을때
+	// 밟을 수 있는 경우는 n-2에서 두칸 올라왓을때 n-3 에서 2칸올라와 n-1에오고 그후 왓을때만
 
-        for (int i = 1; i <= n; i++) {
-            steps[i] = sc.nextInt();
-        }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        if (n == 1) {
-            System.out.println(steps[1]);
-            return;
-        }
-        if (n == 2) {
-            System.out.println(steps[1] + steps[2]);
-            return;
-        }
+		int n = sc.nextInt();
 
-        // 초기값 설정
-        dp[1] = steps[1];
-        dp[2] = steps[1] + steps[2];
-        dp[3] = Math.max(steps[1], steps[2]) + steps[3];
+		int[] steps = new int[n + 1];
+		int[] dp = new int[n + 1];
 
-        for (int i = 4; i <= n; i++) {
-            dp[i] = Math.max(dp[i - 2], dp[i - 3] + steps[i - 1]) + steps[i];
-        }
+		for (int i = 1; i < n + 1; i++) {
+			steps[i] = sc.nextInt();
+		}
+		if (n == 1) {
+		    System.out.println(steps[1]); 
+		    return;
+		}
+		if (n == 2) {
+		    System.out.println(steps[1] + steps[2]); 
+		    return;
+		}
 
-        System.out.println(dp[n]);
-    }
+		dp[1] = steps[1];
+		dp[2] = steps[1] + steps[2];
+
+		for (int i = 3; i < n+1; i++) {
+			dp[i] = Math.max(dp[i - 2], dp[i - 3] + steps[i - 1]) + steps[i];
+		}
+
+		System.out.println(dp[n]);
+
+	}
+
 }
