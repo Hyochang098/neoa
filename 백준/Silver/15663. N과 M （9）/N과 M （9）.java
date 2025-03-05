7a33cccf3353b466122b3a466153f8ecf93d0191
Hyochang098 <wlsgyckd7@naver.com>
2025-03-05 17:04:08 +0900

    [Silver II] Title: N과 M (9), Time: 168 ms, Memory: 21356 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,48 +1,60 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+
 	static int n;
 	static int m;
 	static int[] arr;
 	static boolean[] visited;
+	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		n = sc.nextInt();
-		m = sc.nextInt();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());// 배열 크기
+		m = Integer.parseInt(st.nextToken());// 조합수
 		arr = new int[n];
 		visited = new boolean[n];
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(arr);
-		recursion(0,new int[m]);
-		
-		
 
+		Arrays.sort(arr);
+		recursion(0, new int[m]);
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 
-	private static void recursion(int cur,int[] select) {
+	private static void recursion(int cur, int[] select) {
 		// TODO Auto-generated method stub
-		if(cur==m) {
-			for(int s:select) {
-				System.out.print(s+" ");
+		if (cur == m) {
+			for (int a : select) {
+				sb.append(a).append(" ");
 			}
-			System.out.println();
+			sb.append("\n");
 			return;
 		}
 		int pre = 0;
-		for(int i=0;i<n;i++) {
-			if(!visited[i]&&arr[i]!=pre) {
-				visited[i]=true;
-				select[cur]=arr[i];
-				recursion(cur+1,select);
-				visited[i]=false;
+		for (int i = 0; i < n; i++) {
+			if (!visited[i] && arr[i] != pre) {
+				visited[i] = true;
+				select[cur] = arr[i];
+				recursion(cur + 1, select);
+				visited[i] = false;
 				pre = arr[i];
 			}
 		}
-		
 	}
 }
