d360de7c67fe26bd4790e3980a4995d68dcc87bf
Hyochang098 <wlsgyckd7@naver.com>
2025-03-06 21:42:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 552 ms, Memory: 42084 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,73 +1,55 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class Main {
-
-	static int[][] link;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int n = Integer.parseInt(st.nextToken());// 정점(노드)의 수
-		link = new int[n + 1][n + 1];
-		int m = Integer.parseInt(st.nextToken());// 간선의 수
-
-		int v = Integer.parseInt(st.nextToken());// 시작점
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int v = sc.nextInt();
+		int[][] arr = new int[n + 1][n + 1];
 
 		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int node = Integer.parseInt(st.nextToken());
-			int target = Integer.parseInt(st.nextToken());
-
-			link[node][target] = 1;// 서로 연결이 되어있으니 둘다 1처리
-			link[target][node] = 1;
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[a][b] = 1;
+			arr[b][a] = 1;
 		}
-
-		Stack<Integer> stack = new Stack<>();
-		boolean[] stackv = new boolean[n + 1];
+		Stack<Integer> stack = new Stack<Integer>();
+		boolean[] stackvisited = new boolean[n + 1];
 		stack.add(v);
 		while (!stack.isEmpty()) {
-			int num = stack.pop();
-			if (!stackv[num]) {
-				stackv[num] = true;
-				sb.append(num).append(" ");
-			}
-			for (int i = n; i >= 1; i--) { // 조건에 있는 작은 순서대로 방문
-				if (!stackv[i] && link[num][i] == 1) {
+			int cur = stack.pop();
+			if (stackvisited[cur])
+				continue;
+			else
+				stackvisited[cur]=true;{
+			System.out.print(cur + " ");
+			for (int i = n; i >= 1; i--) {
+				if (arr[cur][i] == 1 && stackvisited[i] == false) {
 					stack.add(i);
-				}
+				}}
 			}
-
 		}
-		sb.append("\n");
 
-		Queue<Integer> que = new ArrayDeque<Integer>();
-		boolean[] queuev = new boolean[n + 1];
-
+		System.out.println();
+		Queue<Integer> que = new LinkedList<Integer>();
+		boolean[] quevisited = new boolean[n + 1];
 		que.add(v);
-		queuev[v]=true;
-		while(!que.isEmpty()) {
-			int num = que.poll();
-			sb.append(num).append(" ");
-			for(int i=1;i<=n;i++) {
-				if(!queuev[i]&&link[num][i]==1) {
+		quevisited[v] = true;
+		while (!que.isEmpty()) {
+			int cur = que.poll();
+			System.out.print(cur + " ");
+			for (int i = 1; i <= n; i++) {
+				if (arr[cur][i] == 1 && quevisited[i] == false) {
 					que.add(i);
-					queuev[i]=true;
+					quevisited[i] = true;
 				}
 			}
 		}
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+
 	}
+
 }
