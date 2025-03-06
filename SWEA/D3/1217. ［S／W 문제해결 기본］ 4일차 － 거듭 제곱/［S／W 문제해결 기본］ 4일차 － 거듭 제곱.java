7e07597fb666df82fcbd206d7f68b8ebe0682df5
Hyochang098 <wlsgyckd7@naver.com>
2025-03-06 13:26:32 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 101 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,20 +1,38 @@
+
 import java.util.Scanner;
 
 public class Solution {
+	// 재귀로 제곱?
+	// m만큼 곱하고 0이되면 1반환?
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		for (int tc = 0; tc < 10; tc++) {
-			int t = sc.nextInt();
+
+		for (int tc = 1; tc <= 10; tc++) {
+			int num = sc.nextInt();
 			int n = sc.nextInt();
 			int m = sc.nextInt();
-			System.out.println("#" + t + " " + mutiple(m, n));
+
+			int ans = pow(n, m);
+
+			System.out.println("#" + num + " " + ans);
+
 		}
+
 	}
 
-	public static int mutiple(int n, int a) {
-		if (n == 0) {
+	static int pow(int n, int m) {
+		// TODO Auto-generated method stub
+		if (m == 0) {
 			return 1;
 		}
-		return a*mutiple(n - 1, a);
+		
+		int num = pow(n,m/2);
+		
+		if(m%2==0) {//짝수면 n 리턴하면될듯?
+			return num*num;
+		}else {
+			return num*num*n;
+		}
+
 	}
 }
