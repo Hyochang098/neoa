3e317507c46bfe79fafaafc3e37a26ba979369f7
Hyochang098 <wlsgyckd7@naver.com>
2025-03-10 23:46:51 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 512 ms, Memory: 58812 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,26 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		HashMap<String, Integer> pokemondic = new HashMap<>();
-		HashMap<Integer, String> numdic = new HashMap<>();
-		for(int i =0; i<m;i++) {
-			String pokemon = sc.next();
-			pokemondic.put(pokemon, i+1);
-			numdic.put(i+1, pokemon);
-		}
-		for(int i =0; i<n;i++) {
-			String quest = sc.next();
-			if(quest.matches("\\d+")) {
-				int a = Integer.parseInt(quest);
-				System.out.println(numdic.get(a));
-			}else {
-				System.out.println(pokemondic.get(quest));
-			}
-		}
-	}
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    HashMap<String, Integer> map = new HashMap<>();
+    HashMap<Integer, String> reverseMap = new HashMap<>();
+
+    for (int i = 0; i < n; i++) {
+      String pokemon = br.readLine();
+      map.put(pokemon, i + 1);
+      reverseMap.put(i + 1, pokemon);
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < m; i++) {
+      String command = br.readLine();
+      if (isNumeric(command)) {
+        sb.append(reverseMap.get(Integer.parseInt(command))).append("\n");
+      } else {
+        sb.append(map.get(command)).append("\n");
+      }
+    }
+    System.out.print(sb);
+  }
+
+  private static boolean isNumeric(String str) {
+    for (char c : str.toCharArray()) {
+      if (!Character.isDigit(c)) return false;
+    }
+    return true;
+  }
 }
