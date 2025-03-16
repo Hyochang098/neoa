3e9763464ea504d8bbfebc54bc2f9ddca71ee839
Hyochang098 <wlsgyckd7@naver.com>
2025-03-16 17:00:19 +0900

    [Silver III] Title: N과 M (2), Time: 200 ms, Memory: 18292 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n, m;
-    static int[] arr;
+	static int n, k;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	static int[] arr, sel;
 
-        n = sc.nextInt(); // 1부터 n까지
-        m = sc.nextInt(); // 조합의 길이
-        arr = new int[m];
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        recursion(0, 1);
-    }
+		n = sc.nextInt();
+		k = sc.nextInt();
 
-    static void recursion(int cur, int start) {
-        if (cur == m) { // 조합의 길이가 m이면 출력
-            for (int i = 0; i < m; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
+		arr = new int[n];
+		sel = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = i + 1;
+		}
 
-        for (int i = start; i <= n; i++) { 
-            arr[cur] = i;
-            recursion(cur + 1, i + 1); 
-        }
-    }
+		recur(0, 0);
+	}
+
+	private static void recur(int cur, int start) {
+		// TODO Auto-generated method stub
+		if (cur == k) {
+			for (int i = 0; i < k; i++) {
+				System.out.print(sel[i] + " ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i < n; i++) {
+			sel[cur] = arr[i];
+			recur(cur + 1, i + 1);
+		}
+	}
 }
