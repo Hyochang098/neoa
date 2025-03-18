4a30de9508f76cb42952ffbe9207ab0366918266
Hyochang098 <wlsgyckd7@naver.com>
2025-03-19 00:03:54 +0900

    [Gold V] Title: 링크와 스타트, Time: 292 ms, Memory: 17384 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,15 +6,12 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int n;
-	static int ans = Integer.MAX_VALUE;
 	static int[][] abil;
-	static boolean[] sel;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		abil = new int[n][n];
-		sel = new boolean[n];
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,41 +19,31 @@ public class Main {
 				abil[i][j] = Integer.parseInt(st.nextToken());
 		}
 
-		for (int i = 1; i <= n / 2; i++) {
-			comb(0, 0, i);
+		int ans = Integer.MAX_VALUE;
+
+		for (int bit = 1; bit < (1 << n); bit++) {
+			int cnt = Integer.bitCount(bit);
+			if (cnt > n / 2)
+				continue;
+
+			int start = 0;
+			int link = 0;
+
+			for (int i = 0; i < n; i++) {
+				for (int j = i + 1; j < n; j++) {
+					if ((bit & (1 << i)) != 0 && (bit & (1 << j)) != 0) {
+						start += abil[i][j] + abil[j][i];
+					} else if ((bit & (1 << i)) == 0 && (bit & (1 << j)) == 0) {
+						link += abil[i][j] + abil[j][i];
+					}
+				}
+			}
+
+			ans = Math.min(ans, Math.abs(start - link));
 			if (ans == 0)
 				break;
 		}
 
 		System.out.println(ans);
-	}
-
-	static void comb(int idx, int cnt, int target) {
-		if (cnt == target) {
-			calc();
-			return;
-		}
-
-		for (int i = idx; i < n; i++) {
-			sel[i] = true;
-			comb(i + 1, cnt + 1, target);
-			sel[i] = false;
-			if (ans == 0)
-				return;
-		}
-	}
-
-	static void calc() {
-		int start = 0;
-		int link = 0;
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = i + 1; j < n; j++) {
-				if (sel[i] && sel[j])
-					start += abil[i][j] + abil[j][i];
-				else if (!sel[i] && !sel[j])
-					link += abil[i][j] + abil[j][i];
-			}
-		}
-		ans = Math.min(ans, Math.abs(start - link));
 	}
 }
