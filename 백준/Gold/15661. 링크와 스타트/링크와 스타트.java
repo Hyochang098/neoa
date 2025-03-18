89957cf04206995a43061bab838e2a79b9836bbf
Hyochang098 <wlsgyckd7@naver.com>
2025-03-18 23:58:33 +0900

    [Gold V] Title: 링크와 스타트, Time: 584 ms, Memory: 19104 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,79 +1,61 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int[][] abil;
-    static ArrayList<Integer> start;
-    static ArrayList<Integer> link;
-    static int ans;
+	static int n;
+	static int ans = Integer.MAX_VALUE;
+	static int[][] abil;
+	static boolean[] sel;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ans = Integer.MAX_VALUE;
-        n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		abil = new int[n][n];
+		sel = new boolean[n];
 
-        start = new ArrayList<>();
-        link = new ArrayList<>();
-        abil = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++)
+				abil[i][j] = Integer.parseInt(st.nextToken());
+		}
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                abil[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+		for (int i = 1; i <= n / 2; i++) {
+			comb(0, 0, i);
+			if (ans == 0)
+				break;
+		}
 
-        // 팀의 최소 크기를 1부터 최대 n-1까지 탐색
-        for (int member = 1; member <= n - 1; member++) {
-            matching(0, member);
-        }
+		System.out.println(ans);
+	}
 
-        System.out.println(ans);
-    }
+	static void comb(int idx, int cnt, int target) {
+		if (cnt == target) {
+			calc();
+			return;
+		}
 
-    static void matching(int cur, int member) {
-        if (start.size() == member) {
-            link.clear();
-            for (int i = 0; i < n; i++) {
-                if (!start.contains(i)) {
-                    link.add(i);
-                }
-            }
-            if (!link.isEmpty()) {
-                verse();
-            }
-            return;
-        }
+		for (int i = idx; i < n; i++) {
+			sel[i] = true;
+			comb(i + 1, cnt + 1, target);
+			sel[i] = false;
+			if (ans == 0)
+				return;
+		}
+	}
 
-        for (int i = cur; i < n; i++) {
-            start.add(i);
-            matching(i + 1, member);
-            start.remove(start.size() - 1);
-        }
-    }
-
-    static void verse() {
-        int s_abil = 0;
-        int l_abil = 0;
-
-        for (int i = 0; i < start.size(); i++) {
-            for (int j = i + 1; j < start.size(); j++) {
-                int pair1 = start.get(i);
-                int pair2 = start.get(j);
-                s_abil += abil[pair1][pair2] + abil[pair2][pair1];
-            }
-        }
-
-        for (int i = 0; i < link.size(); i++) {
-            for (int j = i + 1; j < link.size(); j++) {
-                int pair1_l = link.get(i);
-                int pair2_l = link.get(j);
-                l_abil += abil[pair1_l][pair2_l] + abil[pair2_l][pair1_l];
-            }
-        }
-
-        int temp = Math.abs(l_abil - s_abil);
-        ans = Math.min(ans, temp);
-    }
+	static void calc() {
+		int start = 0;
+		int link = 0;
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				if (sel[i] && sel[j])
+					start += abil[i][j] + abil[j][i];
+				else if (!sel[i] && !sel[j])
+					link += abil[i][j] + abil[j][i];
+			}
+		}
+		ans = Math.min(ans, Math.abs(start - link));
+	}
 }
