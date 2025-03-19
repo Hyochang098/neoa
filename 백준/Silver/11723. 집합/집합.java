b11fcb9b18151db4d2b40489d3bbf1a9a3328e6e
Hyochang098 <wlsgyckd7@naver.com>
2025-03-19 17:28:57 +0900

    [Silver V] Title: 집합, Time: 1052 ms, Memory: 313004 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,60 +1,58 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int n = Integer.parseInt(br.readLine());
-		HashSet<Integer> arr = new HashSet<>();
-
-		for (int i = 0; i < n; i++) {
-			String str = br.readLine();
-			String[] arr1 = str.split(" ");
-			String order = arr1[0];
-
-			switch (order) {
-			case "add":
-				int num1 = Integer.parseInt(arr1[1]);
-				Boolean info = arr.contains(num1);
-				if (info)
-					arr.remove(num1);
-				arr.add(Integer.parseInt(arr1[1]));
-				break;
-			case "remove":
-				int num11 = Integer.parseInt(arr1[1]);
-				Boolean info1 = arr.contains(num11);
-				if (info1)
-					arr.remove(num11);
-				break;
-			case "check":
-				int num2 = Integer.parseInt(arr1[1]);
-				Boolean info11 = arr.contains(num2);
-				sb.append(info11 ? "1\n" : "0\n");
-				break;
-			case "toggle":
-				int num111 = Integer.parseInt(arr1[1]);
-				Boolean info2 = arr.contains(num111);
-				if (info2)
-					arr.remove(num111);
-				else
-					arr.add(num111);
-				break;
-			case "all":
-				arr.clear();
-				arr.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
-				break;
-			case "empty":
-				arr.clear();
-				break;
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb   = new StringBuilder();
+        
+        int M = Integer.parseInt(br.readLine());
+        int S = 0;  // 비트로 관리할 집합
+        
+        for(int i=0; i<M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            int x;
+            
+            switch(command) {
+                case "add":
+                    x = Integer.parseInt(st.nextToken());
+                    // x번째 비트를 1로 설정
+                    S |= (1 << (x-1));
+                    break;
+                    
+                case "remove":
+                    x = Integer.parseInt(st.nextToken());
+                    // x번째 비트를 0으로 설정
+                    S &= ~(1 << (x-1));
+                    break;
+                    
+                case "check":
+                    x = Integer.parseInt(st.nextToken());
+                    // x번째 비트가 1이면 1, 아니면 0
+                    sb.append(((S & (1 << (x-1))) != 0) ? 1 : 0).append("\n");
+                    break;
+                    
+                case "toggle":
+                    x = Integer.parseInt(st.nextToken());
+                    // x번째 비트를 뒤집기
+                    S ^= (1 << (x-1));
+                    break;
+                    
+                case "all":
+                    // 비트 0~19 (20개) 전부 1로 설정
+                    S = (1 << 20) - 1;
+                    break;
+                    
+                case "empty":
+                    // 비트 전부 0으로
+                    S = 0;
+                    break;
+            }
+        }
+        
+        System.out.print(sb);
+    }
 }
