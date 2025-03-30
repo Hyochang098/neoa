15d57636571333594af24a0b3c5041ef40ae4c27
Hyochang098 <wlsgyckd7@naver.com>
2025-03-30 20:32:58 +0900

    [Gold I] Title: 트리의 순회, Time: 1256 ms, Memory: 103392 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.java
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	// 2263 - 트리의 순회
@@ -13,20 +17,22 @@ public class Main {
 	static int[] in, post;
 	static Map<Integer, Integer> map = new HashMap<>();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt();
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 		in = new int[n];
 		post = new int[n];
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			in[i] = sc.nextInt();
-			map.put(in[i], i);
+			int num = Integer.parseInt(st.nextToken());
+			in[i] = num;
+			map.put(num, i);
 		}
 
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			post[i] = sc.nextInt();
+			post[i] = Integer.parseInt(st.nextToken());
 		}
 
 		find(0, n - 1, 0, n - 1);
