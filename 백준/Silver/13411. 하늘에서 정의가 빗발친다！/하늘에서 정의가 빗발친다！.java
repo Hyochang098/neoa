7c64bb48f3ec9d3016b8ed5c0178c14759f6aedd
Hyochang098 <wlsgyckd7@naver.com>
2025-03-31 15:25:30 +0900

    [Silver IV] Title: 하늘에서 정의가 빗발친다!, Time: 616 ms, Memory: 50072 KB -BaekjoonHub

--- 백준/Silver/13411. 하늘에서 정의가 빗발친다！/하늘에서 정의가 빗발친다！.java
+++ 백준/Silver/13411. 하늘에서 정의가 빗발친다！/하늘에서 정의가 빗발친다！.java
@@ -1,27 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+        double[][] robot = new double[n][2];
 
-		double[][] robot = new double[n][2];
-		for (int i = 0; i < n; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			int speed = sc.nextInt();
-			double time = Math.sqrt(x * x + y * y) / speed;
-			robot[i][1] = time;
-			robot[i][0] = i + 1;
-		}
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int speed = Integer.parseInt(st.nextToken());
+            robot[i][1] = Math.sqrt(x * x + y * y) / speed;
+            robot[i][0] = i + 1;
+        }
 
-		Arrays.sort(robot, (a, b) -> Double.compare(a[1], b[1]));
+        Arrays.sort(robot, (a, b) -> Double.compare(a[1], b[1]));
 
-		for (int i = 0; i < n; i++) {
-			System.out.println((int) robot[i][0]);
-		}
-	}
+        for (int i = 0; i < n; i++) {
+            sb.append((int) robot[i][0]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
