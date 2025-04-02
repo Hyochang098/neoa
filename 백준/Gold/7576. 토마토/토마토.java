a7daf9a5cdad1abe9bd8c169a7048d766718e2a3
Hyochang098 <wlsgyckd7@naver.com>
2025-04-02 22:28:39 +0900

    [Gold V] Title: 토마토, Time: 1492 ms, Memory: 337960 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
 
@@ -6,61 +6,54 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int m = sc.nextInt();
 		int n = sc.nextInt();
-		int[][] tomato = new int[n][m];
+		int m = sc.nextInt();
 
-		Queue<int[]> queue = new LinkedList<int[]>();
-		int already = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+		int[][] tomato = new int[m][n];
+		int target = 0;
+
+		Queue<int[]> que = new ArrayDeque<>();
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
 				tomato[i][j] = sc.nextInt();
 				if (tomato[i][j] == 1) {
-					queue.offer(new int[] { i, j });
-				} 
-			}
-		}
-		int ans = bfs(queue, tomato);
-		boolean check=true;
-		Loop:
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+					que.add(new int[] { i, j });
+				}
 				if (tomato[i][j] == 0) {
-					check=false;
-				} 
-			}
-		}
-		if(check)
-			System.out.println(ans);
-		else
-			System.out.println(-1);
-	}
-
-	public static int bfs(Queue<int[]> sample, int[][] arr) {
-		int h = arr.length;
-		int w = arr[0].length;
-
-		int[] dy = new int[] { 1, -1, 0, 0 };
-		int[] dx = new int[] { 0, 0, 1, -1 };
-		int count = -1;
-		while (!sample.isEmpty()) {
-			int size = sample.size();
-			count++;
-			for (int i = 0; i < size; i++) {
-				int[] tt = sample.poll();
-				int y = tt[0];
-				int x = tt[1];
-
-				for (int j = 0; j < 4; j++) {
-					int ny = y + dy[j];
-					int nx = x + dx[j];
-					if (ny >= 0 && ny < h && nx >= 0 && nx < w && arr[ny][nx] == 0) {
-						arr[ny][nx] = 1;
-						sample.offer(new int[] {ny,nx});
-					}
+					target++;
 				}
 			}
 		}
-		return count;
+		if (target == 0) {
+			System.out.println(0);
+			return;
+		}
+
+		int[] dy = { 1, -1, 0, 0 };
+		int[] dx = { 0, 0, -1, 1 };
+		int ans = 0;
+		while (!que.isEmpty() && target != 0) {
+			int s = que.size();
+			for (int a = 0; a < s; a++) {
+				int[] temp = que.poll();
+				int y = temp[0];
+				int x = temp[1];
+				for (int i = 0; i < 4; i++) {
+					int ny = y + dy[i];
+					int nx = x + dx[i];
+					if (ny >= 0 && ny < m && nx >= 0 && nx < n && tomato[ny][nx] == 0) {
+						tomato[ny][nx] = 1;
+						que.add(new int[] { ny, nx });
+						target--;
+					}
+				}
+			}
+			ans++;
+		}
+		if (target != 0) {
+			System.out.println(-1);
+		} else
+			System.out.println(ans);
+
 	}
 }
