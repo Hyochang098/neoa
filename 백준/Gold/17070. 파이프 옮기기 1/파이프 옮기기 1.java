0366f3e44d475d982e658d2de17aca900469f531
Hyochang098 <wlsgyckd7@naver.com>
2025-05-04 22:26:02 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 364 ms, Memory: 172864 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -16,14 +20,18 @@ public class Main {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
 		house = new int[n][n];
-
-		for (int i = 0; i < n; i++)
-			for (int j = 0; j < n; j++)
-				house[i][j] = sc.nextInt();
+		StringTokenizer st;
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				house[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
 		dfs(new pipe(0, 1, 0));
 		System.out.println(count);
