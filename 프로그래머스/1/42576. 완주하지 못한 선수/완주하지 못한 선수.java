2cef89ea85cf48bd6b96d4703c353c02a13316cd
Hyochang098 <wlsgyckd7@naver.com>
2025-06-18 02:38:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 77.67 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,4 +1,39 @@
 class Solution {
+    /**
+     * P42576. 완주하지 못한 선수
+     * 단순하게 방문배열과 비교방법으로는 시간 초과 발생
+     * 참가자 수가 최대 100,000명 -> O(n^2) 시간복잡도는 불가능
+     * 채점 결과
+     * 정확성: 58.3
+     * 효율성: 0.0
+     * 합계: 58.3 / 100.0
+     * 
+     * 해시맵을 이용하여 참가자와 완주자를 카운트하는 방법으로 해결해야함
+     * 해당 문제를 풀려면 해시맵으로 풀어야 시간내에 해결 가능
+     * 라이브러리 사용이 안되므로 해시맵과 필요할만한 기능들을 구현 -> 다른 사람풀이를 보니 사용 가능함
+     * 채점 결과
+     * 정확성: 58.3
+     * 효율성: 41.7
+     * 합계: 100.0 / 100.0
+     * 
+     * 바닐라 자바로 풀어야 하는 상황에서 더 쉽게 풀려고 했다면
+     * 해시맵을 직접 구현하는 것보다 버블정렬을 구현해서 풀어도 풀이가 가능
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for ( i=0; i<completion.length; i++){
+
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
+     */
     class Runner {
         String key;
         int value;
@@ -11,13 +46,13 @@ class Solution {
     }
 
     class CustomHashMap {
-        private final int SIZE = 1000;
+        private final int SIZE = 200003;
         private Runner[] entry = new Runner[SIZE];
 
         private int hash(String key) {
             int hash = 0;
-            for (char c : key.toCharArray()) {
-                hash = (hash * 31 + c) % SIZE;
+            for (int i = 0; i < key.length(); i++) {
+                hash = (hash * 31 + key.charAt(i)) % SIZE;
             }
             return hash;
         }
@@ -34,9 +69,9 @@ class Solution {
                 current = current.next;
             }
 
-            Runner newEntry = new Runner(key, value);
-            newEntry.next = entry[index];
-            entry[index] = newEntry;
+            Runner newRunner = new Runner(key, value);
+            newRunner.next = entry[index];
+            entry[index] = newRunner;
         }
 
         public int get(String key) {
@@ -53,6 +88,11 @@ class Solution {
             return 0;
         }
 
+        /**
+         * 기존 해시맵에는 없는 매서드
+         * 참가자 배열을 순회하면서 해당 키의 값을 1 증가시키는 매서드
+         * 이 매서드는 참가자 배열에 있는 모든 키를 해시맵에 추가하는 역할을 함
+         */
         public void increment(String key) {
             int index = hash(key);
             Runner current = entry[index];
@@ -64,10 +104,13 @@ class Solution {
                 }
                 current = current.next;
             }
-
             put(key, 1);
         }
 
+        /**
+         * 완주자 배열을 순회하면서 해당 키의 값을 1 감소시키는 매서드
+         * 이 매서드는 완주자 배열에 있는 모든 키를 해시맵에서 제거하는 역할을 함
+         */
         public void decrement(String key) {
             int index = hash(key);
             Runner current = entry[index];
@@ -91,7 +134,7 @@ class Solution {
                     current = current.next;
                 }
             }
-            return null;
+            return "error";
         }
     }
 
